34b7cf422690ac28e31666d46ada09c75b80496e
rushabhkashyap <redrider66rdk@gmail.com>
2014-01-03 17:01:49 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php echo "Welcome to the G-app code repository <br/> 
+All files and libraries used for G-app can be accessed here 
+<br/> Gapp is a simple application which logs you in through gmail and displays your contacts" ;
+?>
